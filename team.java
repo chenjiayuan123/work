70429ff680fb6aff17e917c2c738cb1de9af0822
chenjiayuan123 <54424145+chenjiayuan123@users.noreply.github.com>
2019-12-13 09:17:48 +0800

    Add files via upload

---
+++ team.java
@@ -0,0 +1,23 @@
+package team;
+
+import java.util.ArrayList;
+
+public class team {
+
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+
+        int i = 180201;
+
+        for (i = 180201; i < 180241; i++) {
+
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
+
+        ArrayList arr = new ArrayList();
+
+
+
+    }
+}
